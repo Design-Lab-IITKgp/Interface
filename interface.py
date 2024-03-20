d05af24563a694414e90c0fc11090fb579562c5d
Sumajit Maji <108549481+smjt001@users.noreply.github.com>
2024-03-20 17:59:55 +0530

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,30 @@
+import keyboard
+import requests
+import json
+import sounddevice as sd
+
+if __name__ == "__main__":
+    print("Interface Started...")
+    while True:
+        if keyboard.is_pressed("m"):
+            RATE=48000
+            CHANNEL=2
+            DURATION = 5  # seconds
+            print("Recording Audio for "+str(DURATION)+" seconds")
+            myrecording = sd.rec(DURATION * RATE, samplerate=RATE, channels=CHANNEL, dtype='float64')
+            sd.wait()
+            print("Audio recording complete , Play Audio")
+            print(myrecording)
+            base_url = "http://10.145.122.143:5050"
+            
+            response = requests.post(base_url, data={
+                "data": myrecording.tolist()
+            })
+            if response.status_code == 200:
+                print("Request successful!")
+                print("Response:", response.json())
+        
+            else:
+                print("Request failed with status code:", response.status_code)
+                print("Response:", response.text)
+            
